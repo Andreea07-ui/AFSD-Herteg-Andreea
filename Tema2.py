4dd5a8148fc55fa70331d4158c91c39132e10b00
Andreea07-ui <andreeaherteg7@gmail.com>
2025-10-28 16:52:29 +0200

    tema1

--- Tema2.py
+++ Tema2.py
@@ -9,6 +9,30 @@ interogari_nume = ["Ana", "Mara", "Elena", "stop"]
 
 absente = [1, 0, 2, 3, 0]
 
+#A
+for i in range(len(elevi)):
+    print(elevi[i], end=", ")
+    print(note[i])
+
+nota_maxima=(max(note))
+nota_minima=(min(note))
+max_elevi = elevi[note.index(nota_maxima)]
+print(max_elevi, nota_maxima)
+min_elevi = elevi[note.index(nota_minima)]
+print(min_elevi, nota_minima)
+ma=sum(note)/len(note)
+print(round(ma,2))
+
+for i in range(len(elevi)):
+    if note[i] >= 5:
+        print(elevi[i])
+
+#B
+for i in range(len(note)):
+    note[i] = note[i] + 1
+    if note[i] > 10:
+        note[i] = 10
+print(note)
 elevi1=elevi.pop(3)
 elevii=elevi.append(elev_nou)
 print(elevi)
@@ -18,8 +42,7 @@ for i in range(len(elevi)):
     print(elevi[i], end=", ")
     print(note[i])
 
-nota_maxima=(max(note),elevi[2])
-print((nota_maxima)
+
 
 
 
