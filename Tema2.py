9e240e2411f754aa4439d36b12d94b250b667d40
Andreea07-ui <andreeaherteg7@gmail.com>
2025-10-28 17:03:09 +0200

    tema2

--- Tema2.py
+++ Tema2.py
@@ -20,6 +20,7 @@ max_elevi = elevi[note.index(nota_maxima)]
 print(max_elevi, nota_maxima)
 min_elevi = elevi[note.index(nota_minima)]
 print(min_elevi, nota_minima)
+
 ma=sum(note)/len(note)
 print(round(ma,2))
 
@@ -33,14 +34,48 @@ for i in range(len(note)):
     if note[i] > 10:
         note[i] = 10
 print(note)
+
 elevi1=elevi.pop(3)
 elevii=elevi.append(elev_nou)
 print(elevi)
+
 Note=note.append(nota_elev_nou)
 print(note)
+
 for i in range(len(elevi)):
     print(elevi[i], end=", ")
     print(note[i])
+#C
+i = 0
+while i < len(interogari_nume):
+    nume = interogari_nume[i]
+    if nume == "stop":
+        break  # oprește procesarea
+    if nume in elevi:
+        poz = elevi.index(nume)
+        print(f"{nume} are nota {note[poz]}.")
+    else:
+        print(f"{nume} nu există în listă.")
+    i += 1
+
+numar_trecuti = 0
+numar_picati  = 0
+for i in range(len(note)):
+    if note[i] >= 5:
+        numar_trecuti += 1
+    else:
+        numar_picati += 1
+print(f"Elevi cu notă ≥ 5: {numar_trecuti}")
+print(f"Elevi cu notă < 5: {numar_picati}")
+
+note_trecuti = []
+for n in note:
+    if n >= 5:
+        note_trecuti.append(n)
+if len(note_trecuti) > 0:
+    media = sum(note_trecuti) / len(note_trecuti)
+    print(f"Note ≥ 5: {note_trecuti}")
+    print(f"Media notelor ≥ 5: {media:.2f}")
 
 
 
